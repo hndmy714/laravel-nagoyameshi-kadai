87c719d24d3bd7f6b1fb2d07efa7814e54095e7b
hndmy714 <mayu.h.928714@gmail.com>
2025-01-16 21:26:04 +0900

    課題

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Restaurant;
+
+class RestaurantController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $keyword = $request->input('keyword');
+
+        if ($keyword) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")->paginate(15);
+        } else {
+            $restaurants = Restaurant::paginate(15);
+        }
+
+        $total = $restaurants->total();
+
+        return view('admin.restaurants.index', compact('restaurants', 'total', 'keyword'));
+    }
+
+    
+
+    public function show(Restaurant $restaurant)
+    {
+        return view('admin.restaurants.show', compact('restaurant'));
+    }
+
+    public function create()
+    {
+        return view('admin.restaurants.create');
+    }
+
+    public function store(RestaurantRequest $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0'
+        ]);
+
+        $restaurant = new Restaurant();
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image_name = basename($image);
+        } else {
+            $restaurant->image_name = '';
+        }
+
+        $restaurant->save();
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を登録しました。');
+    }
+
+    public function edit($id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        return view('admin.restaurants.edit', compact('restaurant'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required',
+            'image' => 'image|max:2048',
+            'description' => 'required',
+            'lowest_price' => 'required|numeric|min:0|lte:highest_price',
+            'highest_price' => 'required|numeric|min:0|gte:lowest_price',
+            'postal_code' => 'required|digits:7',
+            'address' => 'required',
+            'opening_time' => 'required|before:closing_time',
+            'closing_time' => 'required|after:opening_time',
+            'seating_capacity' => 'required|numeric|min:0'
+        ]);
+
+        $restaurant->name = $request->input('name');
+        $restaurant->description = $request->input('description');
+        $restaurant->lowest_price = $request->input('lowest_price');
+        $restaurant->highest_price = $request->input('highest_price');
+        $restaurant->postal_code = $request->input('postal_code');
+        $restaurant->address = $request->input('address');
+        $restaurant->opening_time = $request->input('opening_time');
+        $restaurant->closing_time = $request->input('closing_time');
+        $restaurant->seating_capacity = $request->input('seating_capacity');
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/restaurants');
+            $restaurant->image_name = basename($image);
+        }
+
+        $restaurant->save();
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を編集しました。');
+    }
+
+    public function destroy(Restaurant $restaurant)
+    {
+        $restaurant->delete();
+
+        return redirect()->route('admin.restaurants.index')->with('flash_message', '店舗を削除しました。');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,13 @@ require __DIR__.'/auth.php';
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
     Route::resource('users', Admin\UserController::class)->only(['index', 'show']);
+
+    Route::resource('restaurants', RestaurantController::class)->except(['index', 'create', 'edit']);
+    Route::get('restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
+    Route::get('restaurants/{restaurant}', [RestaurantController::class, 'show'])->name('restaurants.show');
+    Route::get('restaurants/create', [RestaurantController::class, 'create'])->name('restaurants.create');
+    
+    Route::get('restaurants/edit/{id}', [RestaurantController::class, 'edit'])->name('restaurants.edit');
+    Route::get('restaurants/update', [RestaurantController::class, 'update'])->name('restaurants.update');
+    Route::get('restaurants/destroy', [RestaurantController::class, 'destroy'])->name('restaurants.destroy');
 });
