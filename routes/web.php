1fbb7cf8b953d68ac2c72716eee292d14a70a532
hndmy714 <mayu.h.928714@gmail.com>
2025-01-17 21:51:26 +0900

    管理者側のカテゴリ管理機能エラー

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\RestaurantController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
     Route::resource('users', Admin\UserController::class)->only(['index', 'show']);
 
-    Route::resource('restaurants', Admin\RestaurantController::class);
-    
+    Route::resource('restaurants', Admin\RestaurantController::class);    
+});
+
+Route::prefix('admin')->name('admin.')->middleware('auth:admin')->group(function () {
+    Route::resource('categories', Admin\CategoryController::class);
 });
